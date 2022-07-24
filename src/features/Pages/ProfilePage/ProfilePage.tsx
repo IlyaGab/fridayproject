534151a468d80a75a4e09c6c0c420202ddd42cd7
Dizziby <Dizzi_by@mail.ru>
2022-07-24 18:28:13 +0300

    layout start

--- src/features/Pages/ProfilePage/ProfilePage.tsx
+++ src/features/Pages/ProfilePage/ProfilePage.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import styles from "./profilePage.module.scss"
+import {faArrowRightFromBracket, faPencil} from "@fortawesome/free-solid-svg-icons"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import avatar from "../../../assets/img/avatar.png"
+
 
 export const ProfilePage = () => {
     return (
-        <div>
-            Profile
+        <div className={styles.profilePage}>
+            <div className={styles.profile}>
+                <h1>Personal Information</h1>
+                <img className={styles.avatar} src={avatar} alt={"avatar"} />
+                <p className={styles.name}>Ivan <FontAwesomeIcon icon={faPencil} size="sm" /></p>
+                <p className={styles.email}>j&johnson@gmail.com</p>
+                <button className={styles.btn}><FontAwesomeIcon icon={faArrowRightFromBracket} size="lg" /> Log out</button>
+            </div>
         </div>
     );
 };
