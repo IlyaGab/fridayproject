b534a5e6201470d8c19a24047cbb0232729cd2af
Dizziby <Dizzi_by@mail.ru>
2022-08-01 00:49:17 +0300

    finished pagination

--- src/features/Pages/PacksList/Pagination/Pagination.tsx
+++ src/features/Pages/PacksList/Pagination/Pagination.tsx
@@ -14,20 +14,49 @@ export const Pagination = () => {
 
     const [page, setPage] = useState<number>(1)
     const [pageSize, setPageSize] = useState<number>(arr[0])
+    const [startPage, setStartPage] = useState<number>(1)
+    const [finishPage, setFinishPage] = useState<number>(5)
+
 
     const numberOfPages = Math.ceil(cardPacksTotalCount / pageSize)
 
     let pages = [];
-    for (let i = 1; i <= 5; i++) {
+    for (let i = startPage; i <= finishPage; i++) {
         pages.push(i)
     }
-    const onClickHandler = (el: number) => {
+    const onClickPageHandler = (el: number) => {
         setPage(el)
     }
 
-    const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+    const onChangeSizePageHandler = (e: ChangeEvent<HTMLSelectElement>) => {
         debugger
         setPageSize(Number(e.currentTarget.value))
+    }
+
+    const onClickBackHandler = () => {
+        if(startPage - 5 < 1) {
+            setStartPage(1)
+            setFinishPage(5)
+        } else {
+            setStartPage(startPage - 5)
+            setFinishPage(finishPage - 5)
+        }
+
+    }
+
+    const onClickForthHandler = () => {
+        if(finishPage + 5 > numberOfPages) {
+            setStartPage(numberOfPages - 5)
+            setFinishPage(numberOfPages - 1)
+        } else {
+            setStartPage(startPage + 5)
+            setFinishPage(finishPage + 5)
+        }
+    }
+
+    let ellipsis = "..."
+    if(finishPage === numberOfPages - 1) {
+        ellipsis = ""
     }
 
     useEffect(() => {
@@ -36,19 +65,26 @@ export const Pagination = () => {
 
     return (
         <div className={styles.pagination}>
-            <div>
-                <FontAwesomeIcon className={styles.icon} icon={faAngleLeft} size="lg"/>
-                {pages.map(el => <span key={el} className={styles.pageButton}
-                                       onClick={() => onClickHandler(el)}>{el}</span>)}
-                ...
-                {numberOfPages}
-                <FontAwesomeIcon className={styles.icon} icon={faAngleRight} size="lg"/>
-                Show
-                <select onChange={onChangeHandler}>
-                    {arr.map(el => <option key={el} value={el}>{el}</option>)}
-                </select>
-                Cards per Page
-            </div>
+            <button onClick={onClickBackHandler}><FontAwesomeIcon className={styles.icon}
+                                                                  icon={faAngleLeft} size="lg"/>
+            </button>
+
+            {pages.map(el => <span key={el}
+                                   className={page === el ? `${styles.pageButton} ${styles.activePageButton}` : `${styles.pageButton}`}
+                                   onClick={() => onClickPageHandler(el)}>{el}</span>)}
+            {}
+            <span>{ellipsis}</span>
+            <span key={numberOfPages}
+                  className={page === numberOfPages ? `${styles.pageButton} ${styles.activePageButton}` : `${styles.pageButton}`}
+                  onClick={() => onClickPageHandler(numberOfPages)}>{numberOfPages}</span>
+            <button onClick={onClickForthHandler}><FontAwesomeIcon className={styles.icon}
+                                                                   icon={faAngleRight} size="lg"/>
+            </button>
+            Show
+            <select onChange={onChangeSizePageHandler}>
+                {arr.map(el => <option key={el} value={el}>{el}</option>)}
+            </select>
+            Cards per Page
         </div>
     )
 }
