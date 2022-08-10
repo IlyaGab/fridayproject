fe1cf6c1359b8a3b52c4f4bdae2647987490db0c
Dizziby <Dizzi_by@mail.ru>
2022-08-10 23:56:54 +0300

    refactor components Menu in HeaderCardsList

--- src/features/Pages/CardsList/HeaderCardsList/HeaderCardsList.tsx
+++ src/features/Pages/CardsList/HeaderCardsList/HeaderCardsList.tsx
@@ -1,10 +1,10 @@
-import React, {ReactElement, useState} from 'react';
-import {useAppSelector} from '../../../../common/hooks/useAppSelector';
-import styles from './headerCardsList.module.scss'
-import {faEllipsisVertical} from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {Menu} from './Menu/Menu';
-import {AddNewCardModal} from '../../../Modals/AddNewCardModal/AddNewCardModal';
+import React, {ReactElement, useState} from "react";
+import {useAppSelector} from "../../../../common/hooks/useAppSelector";
+import styles from "./headerCardsList.module.scss"
+import {faEllipsisVertical} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {Menu} from "./Menu/Menu";
+import {AddNewCardModal} from "../../../Modals/AddNewCardModal/AddNewCardModal";
 
 export const HeaderPacksList = (): ReactElement => {
 
@@ -15,14 +15,20 @@ export const HeaderPacksList = (): ReactElement => {
 
     const [showMenu, setShowMenu] = useState<boolean>(false)
 
-    const handleSetShowMenu = () => {
+    const onClickSetShowMenu = () => {
         setShowMenu(!showMenu)
     }
+    const onBlurSetShowMenu = () => {
+        setTimeout(() =>{
+            setShowMenu(false)
+        }, 200)
+    }
+
     return (
         <div className={styles.header}>
             <h2>
                 {packName} {isMyCards &&
-                <button className={styles.btnMenu} onClick={handleSetShowMenu}>
+                <button className={styles.btnMenu} onClick={onClickSetShowMenu} onBlur={onBlurSetShowMenu}>
                     <FontAwesomeIcon className={styles.icon} icon={faEllipsisVertical} size="sm"/>
                 </button>}
                 {showMenu && <Menu/>}
