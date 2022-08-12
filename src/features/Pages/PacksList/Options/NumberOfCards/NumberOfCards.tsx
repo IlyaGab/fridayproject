8e7f65de10ec7ffc282a13fcdc3e39bee999c996
Dizziby <Dizzi_by@mail.ru>
2022-08-12 22:40:23 +0300

    refactor ShowPacksCards

--- src/features/Pages/PacksList/Options/NumberOfCards/NumberOfCards.tsx
+++ src/features/Pages/PacksList/Options/NumberOfCards/NumberOfCards.tsx
@@ -1,9 +1,9 @@
 import {Slider} from "@mui/material";
 import React, {ReactElement, useEffect} from "react";
 import {useAppDispatch} from "../../../../../common/hooks/useAppDispatch";
-import {setQueryParamsAC} from '../../packsListReducer';
+import {setQueryParamsAC} from "../../packsListReducer";
 import styles from "./numberOfCards.module.scss";
-import {useSearchParams} from 'react-router-dom';
+import {useSearchParams} from "react-router-dom";
 
 const min = 0
 const max = 110
@@ -13,24 +13,24 @@ export const NumberOfCards = (): ReactElement => {
 
     const [search, setSearch] = useSearchParams()
 
-    const minValue = Number(search.get('min')) || min
-    const maxValue = Number(search.get('max')) || max
+    const minValue = Number(search.get("min")) || min
+    const maxValue = Number(search.get("max")) || max
 
-    const [value, setValue] = React.useState<number[]>([minValue,maxValue]);
+    const [value, setValue] = React.useState<number[]>([minValue, maxValue]);
 
     function valueText(value: number) {
         return `${{value}}`
     }
 
-    const handleChangeCommitted = (event:  React.SyntheticEvent | Event, newValue: number | number[]): void => {
+    const handleChangeCommitted = (event: React.SyntheticEvent | Event, newValue: number | number[]): void => {
         setValue(newValue as number[])
-        search.set(`min` , `${value[0]}`)
-        search.set(`max` , `${value[1]}`)
+        search.set(`min`, `${value[0]}`)
+        search.set(`max`, `${value[1]}`)
         setSearch(search)
     }
 
     useEffect(() => {
-            dispatch(setQueryParamsAC({min: minValue, max: maxValue}))
+        dispatch(setQueryParamsAC({min: minValue, max: maxValue}))
     }, [dispatch, minValue, maxValue])
 
     return (
--- src/features/Pages/PacksList/Options/ShowPacksCards/ShowPacksCards.tsx
+++ src/features/Pages/PacksList/Options/ShowPacksCards/ShowPacksCards.tsx
@@ -1,56 +1,47 @@
-import React, {ReactElement, useEffect} from 'react';
+import React, {ReactElement, useEffect} from "react";
 import {useAppDispatch} from "../../../../../common/hooks/useAppDispatch";
-import {setIsMyCardsPackAC, setQueryParamsAC} from "../../packsListReducer";
+import {setQueryParamsAC} from "../../packsListReducer";
 import styles from "./showPacksCards.module.scss";
 import {useAppSelector} from "../../../../../common/hooks/useAppSelector";
-import { useSearchParams} from 'react-router-dom';
+import {useSearchParams} from "react-router-dom";
 
 export const ShowPacksCards = (): ReactElement => {
     const dispatch = useAppDispatch()
 
-    const isMyCardsPack = useAppSelector(state => state.packsList.isMyCardsPack)
     const user_id = useAppSelector(state => state.profileReducer._id)
     const status = useAppSelector(state => state.appReducer.status)
 
     const [searchParams, setSearchParams] = useSearchParams()
 
+    const userID = searchParams.get("user_id") || ""
+
     const handleShowMyCards = () => {
         searchParams.set('user_id', user_id)
         setSearchParams(searchParams)
         dispatch(setQueryParamsAC({user_id}))
-        dispatch(setIsMyCardsPackAC(true))
     }
 
     const handleShowAllCards = () => {
         searchParams.set('user_id', "")
         setSearchParams(searchParams)
         dispatch(setQueryParamsAC({user_id: ""}))
-        dispatch(setIsMyCardsPackAC(false))
     }
 
     useEffect(()=> {
-        if(searchParams.get("user_id") === "") {
-            dispatch(setQueryParamsAC({user_id: ""}))
-            dispatch(setIsMyCardsPackAC(false))
-        } else {
-            dispatch(setQueryParamsAC({user_id}))
-            dispatch(setIsMyCardsPackAC(true))
-        }
-    },[dispatch, searchParams, user_id])
-
-
+        dispatch(setQueryParamsAC({user_id: userID}))
+    },[dispatch, userID])
 
     return (
         <div className={styles.showPacksCards}>
             <h3>
                 Show packs cards
             </h3>
-            <button className={isMyCardsPack ? `${styles.btn} ${styles.btnActive}` : styles.btn}
+            <button className={userID ? `${styles.btn} ${styles.btnActive}` : styles.btn}
                     onClick={handleShowMyCards}
                     disabled={status === "loading"}
             >My
             </button>
-            <button className={!isMyCardsPack ? `${styles.btn} ${styles.btnActive}` : styles.btn}
+            <button className={!userID ? `${styles.btn} ${styles.btnActive}` : styles.btn}
                     onClick={handleShowAllCards}
                     disabled={status === "loading"}
             >All
