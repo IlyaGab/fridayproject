0387eb5462da5f1fddb636c7b802b5327f91755c
Sheikh <sheihdog325@gmail.com>
2022-08-23 15:45:30 +0300

    fix search

--- src/features/Pages/PacksList/Options/Options.tsx
+++ src/features/Pages/PacksList/Options/Options.tsx
@@ -3,15 +3,14 @@ import styles from "./options.module.scss";
 import {Search} from "../../../../common/components/Search/Search";
 import {ShowPacksCards} from "./ShowPacksCards/ShowPacksCards";
 import {NumberOfCards} from "./NumberOfCards/NumberOfCards";
-import {getPackListTC, setQueryParamsAC} from "../packsListReducer";
+import {setQueryParamsAC} from "../packsListReducer";
 import {useAppDispatch} from "../../../../common/hooks/useAppDispatch";
 
 export const Options = (): ReactElement => {
     const dispatch = useAppDispatch()
 
-    const setSearchPackName = (searchName: string) => {
+    const setSearchPackName = (searchName: string): void => {
         dispatch(setQueryParamsAC({packName: searchName}))
-        dispatch(getPackListTC())
     }
 
     return (
